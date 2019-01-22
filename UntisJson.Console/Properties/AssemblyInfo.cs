46f741d68162ff1ee1b0583478b9af8c4085977e
Marcel Marnitz <marcelmarnitz@googlemail.com>
2019-01-22 20:12:00 +0100

    Update copyright / pack all files into one exe

--- UntisJson.Console/Properties/AssemblyInfo.cs
+++ UntisJson.Console/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("UntisJson.Console")]
+[assembly: AssemblyTitle("UntisJson")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UntisJson.Console")]
-[assembly: AssemblyCopyright("Copyright © Marcel Marnitz 2015")]
+[assembly: AssemblyProduct("UntisJson")]
+[assembly: AssemblyCopyright("Copyright © SchulIT 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
